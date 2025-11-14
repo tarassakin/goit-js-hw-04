74646056dcb5b7a5c2847010163e97840870d3ca
Taras <tarassakin@gmail.com>
2025-11-14 09:06:53 -0800

    hw-js-04

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,25 @@
-function filterArray(numbers, value) {
-  let result = [];
-  for (let index = 0; index < numbers.length; index++) {
-    if (numbers[index] > value) {
-      result.push(numbers[index]);
-    }
-  }
-  return result;
-}
+const profile = {
+  username: 'Jacob',
+  playTime: 300,
+  changeUsername: function (newName) {
+    this.username = newName;
+  },
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+  updatePlayTime: function (hours) {
+    this.playTime = this.playTime + hours;
+  },
+
+  getInfo: function () {
+    // username;
+    // playTime;
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+};
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername('Marco');
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
